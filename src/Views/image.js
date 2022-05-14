65356dea5f430fbc35b8ad6e16913f3c34faf332
TomPoupi <tom.poupard@insa-lyon.fr>
2022-05-14 22:57:42 +0200

    coucou2

---
+++ src/Views/image.js
@@ -0,0 +1,69 @@
+import React,{useEffect,useState} from 'react';
+import {getOneUser,getListUsers} from '../BackRequest/list_request.js';
+ 
+export default function Image() {
+
+
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
+  const [loadingUsers, setLoadingUsers] = useState(true);
+  const [loadingUser, setLoadingUser] = useState(true);
+  const [error, setError] = useState(false);
+  const id = 1;
+
+  useEffect(() => {
+        getOneUser(id)
+            .then((res) => {
+                setUser([res.data]);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+                setError(true);
+            })
+            .finally(() => {
+                setLoadingUser(false);
+            }); 
+    }, [id]);
+
+    useEffect(() => {
+        getListUsers()
+            .then((res) => {
+                setUsers(res.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+                setError(true);
+            })
+            .finally(() => {
+                setLoadingUsers(false);
+            }); 
+    }, [id]);
+
+ 
+ if (loadingUsers || loadingUser ) return 'Loading...';
+ if (error) return 'Error!';
+ 
+  return (
+    <div>
+      <h1>--Image--</h1>
+      {console.log(user)}
+
+        <h2>getListUsers : </h2><br/>      
+        {users.map((user) => (
+          <div>
+          {user.name}
+          </div>
+        ))}
+
+        <br/>
+
+        <h2>getOneUser (id = 1) : </h2> <br/>      
+        {user.map((user) => (
+          <div>
+          {user.name}
+          </div>
+        ))}
+
+    </div>
+  );
+}
